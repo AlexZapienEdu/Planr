439f8f7925f44340186719c4e5b93ea4b7c01530
AlexZapienEdu <alexzapien@uchicago.edu>
2022-08-13 22:29:50 +0000

    Planner: All Random Sections

--- app/controllers/planner_controller.rb
+++ app/controllers/planner_controller.rb
@@ -24,22 +24,38 @@ class PlannerController < ApplicationController
     event = event_with_cat.event_name
 
     #########
-    
+
     #Random Dinner Section
     #Takes in the Category Name and matches it to the ID in the Category Table
-    input_cat = "Fun" #params.fetch("query_cat1")
-    matching_categories1 = Category.where({ :category_name => input_cat}).at(0)
-    cat_id = matching_categories1.id
+    input_cat2 = "Fun" #params.fetch("query_cat2")
+    matching_categories2 = Category.where({ :category_name => input_cat2}).at(0)
+    cat_id2 = matching_categories2.id
 
-    #Finds how many events are in this category
-    matching_category_count = matching_categories1.events_count
+    #Finds how many dinner spots are in this category
+    matching_category_count2 = matching_categories2.dinner_spots_count
+
+    #Returns a random dinner spot within this category
+    matching_dinner_spots_with_cat = DinnerSpot.where({ :category_id => cat_id2})
+    dinner_with_cat = matching_dinner_spots_with_cat.at(rand(matching_category_count2))
+    dinner = dinner_with_cat.loc_name
+
+    #########
+
+    #Random Dessert Section
+    #Takes in the Category Name and matches it to the ID in the Category Table
+    input_cat3 = "Fun" #params.fetch("query_cat3")
+    matching_categories3 = Category.where({ :category_name => input_cat3}).at(0)
+    cat_id3 = matching_categories3.id
+
+    #Finds how many dinner spots are in this category
+    matching_category_count3 = matching_categories3.dessert_spots_count
 
     #Returns a random event within this category
-    matching_events_with_cat = EventSpot.where({ :category_id => cat_id})
-    event_with_cat = matching_events_with_cat.at(rand(matching_category_count))
-    event = event_with_cat.event_name
-    dessert_cat = params.fetch("query_cat3")
-    
+    matching_dessert_spots_with_cat = DessertSpot.where({ :category_id => cat_id3})
+    dessert_with_cat = matching_dessert_spots_with_cat.at(rand(matching_category_count3))
+    dessert = dessert_with_cat.loc_name
+
+    dfadf
   end
 
 
