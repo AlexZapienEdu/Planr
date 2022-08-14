fa69586df89d5715a1f84a2e9d35ab4e80303397
AlexZapienEdu <alexzapien@uchicago.edu>
2022-08-14 00:23:27 +0000

    Displays Recommendations

--- app/controllers/planner_controller.rb
+++ app/controllers/planner_controller.rb
@@ -21,8 +21,8 @@ class PlannerController < ApplicationController
 
     #Returns a random event within this category
     matching_events_with_cat = EventSpot.where({ :category_id => cat_id})
-    event_with_cat = matching_events_with_cat.at(rand(matching_category_count))
-    event = event_with_cat.event_name
+    @event_with_cat = matching_events_with_cat.at(rand(matching_category_count))
+    @event = @event_with_cat.event_name
 
     #########
 
@@ -37,8 +37,8 @@ class PlannerController < ApplicationController
 
     #Returns a random dinner spot within this category
     matching_dinner_spots_with_cat = DinnerSpot.where({ :category_id => cat_id2})
-    dinner_with_cat = matching_dinner_spots_with_cat.at(rand(matching_category_count2))
-    dinner = dinner_with_cat.loc_name
+    @dinner_with_cat = matching_dinner_spots_with_cat.at(rand(matching_category_count2))
+    @dinner = @dinner_with_cat.loc_name
 
     #########
 
@@ -53,10 +53,10 @@ class PlannerController < ApplicationController
 
     #Returns a random event within this category
     matching_dessert_spots_with_cat = DessertSpot.where({ :category_id => cat_id3})
-    dessert_with_cat = matching_dessert_spots_with_cat.at(rand(matching_category_count3))
-    dessert = dessert_with_cat.loc_name
+    @dessert_with_cat = matching_dessert_spots_with_cat.at(rand(matching_category_count3))
+    @dessert = @dessert_with_cat.loc_name
 
-    render({ :template => "categories/index.html.erb" })
+    render({ :template => "plan/planned.html.erb" })
   end
 
 
